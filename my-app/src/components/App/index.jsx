30dab740002ab2af69b03d6c48041742991efc82
AGOSTINA BOTTARINI <47866440@ort>
2024-06-19 10:30:21 -0300

    REVISAR: comon guaradr la cita y hacer la card

--- my-app/src/components/App/index.jsx
+++ my-app/src/components/App/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import FormularioCita from '../FormularioCita';
+import ListaCitas from '../ListaCitas';
 
 const App = () =>
 {
@@ -10,12 +11,22 @@ const App = () =>
         <div style={{display: "flex"}}>
         
             <div className='primera-mitad'>
-              <h1 style={{marginLeft: "14vw", textAlign: "center", textAlign: "center"}}>Formulario de Citas</h1>
+              <h1 style={{marginLeft: "14vw", textAlign: "center"}}>Formulario de Citas</h1>
               <FormularioCita setCitas={setCitas} />
                 
             </div>
             <div className='segunda-mitad'>
               <h1>Listado citas</h1>
+              {console.log(citas)}
+              {citas.forEach(element => {
+                <ListaCitas
+                nombreMascota = {element.nombreMascota}
+                nombreDuenio = {element.nombreDuenio}
+                fecha = {element.fecha}
+                hora = {element.hora}
+                sintomas = {element.sintomas}
+                />
+              })}
             </div>
 
         </div>
--- my-app/src/components/FormularioCita/index.jsx
+++ my-app/src/components/FormularioCita/index.jsx
@@ -1,39 +1,45 @@
 import React, { useState } from 'react';
+import { useEffect } from 'react';
 
 const FormularioCita =  ({ setCitas }) => {
     
-    const [nombre, setNombre] = useState('')
+    const [nombreMascota, setNombre] = useState('')
+    const [nombreDuenio, setNombreD] = useState('')
     const [fecha, setFecha] = useState('')
     const [hora, setHora] = useState('')
+    const [sintomas, setSintomas] = useState('')
+    const [readyToSubmit, setReady] = useState(false)
+    useEffect(() => {
+        handleSubmit()
+    }, [readyToSubmit])
 
-    const handleSubmit = (def) => {
-        def.preventDefault()
+    const handleSubmit = () => {
         const nuevaCita = {
-            nombre,
+            nombreMascota,
+            nombreDuenio,
             fecha,
-            hora
+            hora,
+            sintomas
         }
         setCitas(prevCitas => [...prevCitas, nuevaCita]);
-        setNombre('')
-        setFecha('')
-        setHora('')
+        console.log(nuevaCita)
     }
 
     return (
         <div style={{width: "50%", marginLeft: "19.3vw"}}>
             <div>
-            <form style={{display: "flex", flexDirection: "column", marginBottom: "5vw"}}>
+            <form style={{display: "flex", flexDirection: "column", marginBottom: "5vw"}} onSubmit={() => {setReady(true)}}>
               <label >Nombre Mascota</label>
-              <input type="text" name="mascota" class="u-full-width" placeholder="Nombre Mascota" value=""></input>
+              <input type="text" name="mascota" className="u-full-width" placeholder="Nombre Mascota" onChange={(e) => {setNombre(e.target.value)}}></input>
               <label>Nombre Dueño</label>
-              <input type="text" name="propietario" class="u-full-width" placeholder="Nombre dueño de la mascota" value=""></input>
+              <input type="text" name="propietario" className="u-full-width" placeholder="Nombre dueño de la mascota" onChange={(e) => {setNombreD(e.target.value)}}></input>
               <label>Fecha</label>
-              <input type="date" name="fecha" class="u-full-width" value=""></input>
+              <input type="date" name="fecha" className="u-full-width" onChange={(e) => {setFecha(e.target.value)}}></input>
               <label>Hora</label>
-              <input type="time" name="hora" class="u-full-width" value=""></input>
+              <input type="time" name="hora" className="u-full-width" onChange={(e) => {setHora(e.target.value)}}></input>
               <label>Sintomas</label>
-              <textarea name="sintomas" class="u-full-width" style={{resize: "4vw", marginBottom: "2vw"}}></textarea>
-              <button type="submit" class="u-full-width button-primary">Agregar Cita</button>
+              <textarea name="sintomas" className="u-full-width" style={{resize: "4vw", marginBottom: "2vw"}} onChange={(e) => {setSintomas(e.target.value)}}></textarea>
+              <button type="submit" className="u-full-width button-primary">Agregar Cita</button>
 
             </form>
             </div>
