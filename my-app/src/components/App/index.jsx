697aa04ab005fb40cd6af4f0c0e21aa8fbf41783
AGOSTINA BOTTARINI <47866440@ort>
2024-06-24 08:25:09 -0300

    FUNCIONA EL FORM Y CARGA LAS CARDS

--- my-app/src/components/App/index.jsx
+++ my-app/src/components/App/index.jsx
@@ -2,38 +2,33 @@ import React, { useState } from 'react';
 import FormularioCita from '../FormularioCita';
 import ListaCitas from '../ListaCitas';
 
-const App = () =>
-{
+const App = () => {
     const [citas, setCitas] = useState([]);
+
     return (
-      <section>
-        <h1 style={{textAlign: "center", marginTop: "3vw", marginBottom: "4vw"}}>GESTOR DE CITAS</h1>
-        <div style={{display: "flex"}}>
-        
-            <div className='primera-mitad'>
-              <h1 style={{marginLeft: "14vw", textAlign: "center"}}>Formulario de Citas</h1>
-              <FormularioCita setCitas={setCitas} />
-                
+        <section>
+            <h1 style={{ textAlign: "center", marginTop: "3vw", marginBottom: "4vw" }}>GESTOR DE CITAS</h1>
+            <div style={{ display: "flex" }}>
+                <div className='primera-mitad'>
+                    <h1 style={{ marginLeft: "14vw", textAlign: "center" }}>Formulario de Citas</h1>
+                    <FormularioCita setCitas={setCitas} />
+                </div>
+                <div className='segunda-mitad'>
+                    <h1>Listado citas</h1>
+                    {citas.map((cita, index) => (
+                        <ListaCitas
+                            key={index} // Usamos el índice como clave única temporal
+                            nombreMascota={cita.nombreMascota}
+                            nombreDuenio={cita.nombreDuenio}
+                            fecha={cita.fecha}
+                            hora={cita.hora}
+                            sintomas={cita.sintomas}
+                        />
+                    ))}
+                </div>
             </div>
-            <div className='segunda-mitad'>
-              <h1>Listado citas</h1>
-              {console.log(citas)}
-              {citas.forEach(element => {
-                <ListaCitas
-                nombreMascota = {element.nombreMascota}
-                nombreDuenio = {element.nombreDuenio}
-                fecha = {element.fecha}
-                hora = {element.hora}
-                sintomas = {element.sintomas}
-                />
-              })}
-            </div>
+        </section>
+    );
+};
 
-        </div>
-
-      </section>
-        
-      );
-}
-
-export default App
+export default App;
